b6f930e670c255e4f566b2923863c7ff04d9c5ba
Marijke <marijkemeersman@gmail.com>
2021-05-22 16:30:17 +0200

    navigation done and added bootstrap

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,30 +4,30 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-window.onscroll = function() {makeSticky();};
+// window.onscroll = function() {makeSticky();};
 
-var stickyHeader = document.getElementById("stickyHeader");
-var offsetTopStickyHeader = stickyHeader.offsetTop;
+// var stickyHeader = document.getElementById("stickyHeader");
+// var offsetTopStickyHeader = stickyHeader.offsetTop;
 
-function makeSticky() {
-  if (window.pageYOffset > offsetTopStickyHeader) {
-    stickyHeader.classList.add("sticky");
-  } else {
-    stickyHeader.classList.remove("sticky");
-  }
-};
+// function makeSticky() {
+//   if (window.pageYOffset > offsetTopStickyHeader) {
+//     stickyHeader.classList.add("sticky");
+//   } else {
+//     stickyHeader.classList.remove("sticky");
+//   }
+// };
 
-function openCity(evt, cityName) {
-  console.log("opencity");
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-};
+// function openCity(evt, cityName) {
+//   console.log("opencity");
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// };
